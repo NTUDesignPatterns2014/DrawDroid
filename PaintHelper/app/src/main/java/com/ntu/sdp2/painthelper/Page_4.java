6ef56bdef22108813a3d7c0abec240054db713d8
Lou <tuntun0847@gmail.com>
2014-12-12 13:45:10 +0800

    Add Setting List in Page 4 but Account_Info transaction not working perfectly

--- PaintHelper/app/src/main/java/com/ntu/sdp2/painthelper/Page_4.java
+++ PaintHelper/app/src/main/java/com/ntu/sdp2/painthelper/Page_4.java
@@ -3,18 +3,63 @@ package com.ntu.sdp2.painthelper;
 /**
  * Created by JimmyPrime on 2014/10/26.
  */
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class Page_4 extends Fragment {
+import com.ntu.sdp2.painthelper.settings.Account_Info;
+
+public class Page_4 extends ListFragment {
+    static final String[] itemList = {"Account", "Default Tab", "About"};
+    //static Account_Info account_info = new Account_Info();
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View page_4 = inflater.inflate(R.layout.page_4_frag, container, false);
-        // ((TextView)page_4.findViewById(R.id.textView)).setText("Page 4");
-        return page_4;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*if( savedInstanceState == null ) {
+            if (!account_info.isAdded()) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(0, account_info);
+                fragmentTransaction.commit();
+            }
+        }
+        */
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>((Activity)getActivity(), android.R.layout.simple_list_item_1, itemList);
+        setListAdapter(itemAdapter);
+
+
+    }
+
+    //TODO: Something wrong here....fragment transaction does not work every time.
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        switch(position){
+            case 0:
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.pager, new Account_Info());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                fragmentManager.executePendingTransactions();
+                //Toast.makeText((Activity)getActivity(), "add!!", Toast.LENGTH_LONG).show();
+
+        }
+
     }
 }
