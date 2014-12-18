6be1b874ac0487762b59cf617eb8c03de1ea6779
Jimmy Lee <b00901058@ntu.edu.tw>
2014-12-19 03:52:35 +0800

    corner button 0 is popping itemselector now images cannot be dragged

--- PaintHelper/app/src/main/java/com/ntu/sdp2/painthelper/Page_1.java
+++ PaintHelper/app/src/main/java/com/ntu/sdp2/painthelper/Page_1.java
@@ -5,17 +5,68 @@ package com.ntu.sdp2.painthelper;
  */
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.capricorn.ArcMenu;
+import com.ntu.sdp2.painthelper.Painter.ImageAdapter;
 
 public class Page_1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View page_1 = inflater.inflate(R.layout.page_1_frag, container, false);
-        ((TextView)page_1.findViewById(R.id.textView)).setText("Page 1");
+        final View page_1 = inflater.inflate(R.layout.page_1_frag, container, false);
+
+        final GridView gridview = (GridView)page_1.findViewById(R.id.gridView);
+        final ImageAdapter imageAdapter = new ImageAdapter(this.getActivity());
+        gridview.setAdapter(imageAdapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                //Toast.makeText(page_1.getContext(), "" + position, Toast.LENGTH_SHORT).show();
+                ImageView imageView = (ImageView) imageAdapter.getItem(position);
+                RelativeLayout relativeLayout = (RelativeLayout) page_1.findViewById(R.id.relativeLayout);
+                relativeLayout.addView(imageView);
+                relativeLayout.removeViewInLayout(gridview);
+            }
+        });
+        RelativeLayout relativeLayout = (RelativeLayout) page_1.findViewById(R.id.relativeLayout);
+        relativeLayout.removeViewInLayout(gridview);
+
+        final ArcMenu cornerButton = (ArcMenu)page_1.findViewById(R.id.arcMenu);
+        int[] images = {
+                R.drawable.composer_camera,
+                R.drawable.composer_music,
+                R.drawable.composer_place,
+                R.drawable.composer_sleep,
+                R.drawable.composer_with};
+        final int itemCount = images.length;
+        for (int i=0; i<itemCount; i++) {
+            ImageView item = new ImageView(this.getActivity());
+            item.setImageResource(images[i]);
+            final int position = i;
+            cornerButton.addItem(item, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(page_1.getContext(), "" + position, Toast.LENGTH_SHORT).show();
+                    if (position == 0) {
+                        RelativeLayout relativeLayout = (RelativeLayout) page_1.findViewById(R.id.relativeLayout);
+                        relativeLayout.addView(gridview);
+                        relativeLayout.bringChildToFront(gridview);
+                    }
+                }
+            });
+        }
+
         return page_1;
     }
 }
---
+++ PaintHelper/app/src/main/java/com/ntu/sdp2/painthelper/Painter/ImageAdapter.java
@@ -0,0 +1,68 @@
+package com.ntu.sdp2.painthelper.Painter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.ntu.sdp2.painthelper.R;
+
+/**
+ * Created by JimmyPrime on 2014/12/18.
+ */
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(190, 190));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(4, 4, 4, 4);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+}
