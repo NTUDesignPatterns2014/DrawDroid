6adc64fc4e8c064b555ba8afa48afc19c51a6ad7
WeiTang114 <weitang114@gmail.com>
2014-12-27 16:03:01 +0800

    Image process init commit

---
+++ PaintHelper/app/src/main/java/com/ntu/sdp2/painthelper/utils/SketchImage.java
@@ -0,0 +1,151 @@
+package com.ntu.sdp2.painthelper.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by WeiTang114 on 2014/12/22.
+ */
+public class SketchImage {
+    public static final int WIDTH = 400;
+    public static final int HEIGHT = 400;
+
+    private Bitmap mBmpOriginal = null;
+    private Bitmap mBmp = null;
+
+    /**
+     * The dilation level in non-negative integer. default: 0.
+     */
+    private int mDilateLevel = 0;
+    private int mWidth = 0;
+    private int mHeight = 0;
+
+    public Bitmap getBitmap() {
+        return mBmp;
+    }
+    public int getWidth() {
+        return mWidth;
+    }
+    public int getHeight() { return mHeight; }
+
+
+    public void resize(int width, int height) {
+        // TODO
+        if (width < getWidth() || height < getHeight()) {
+            //  dilate
+            // TODO: check how much is necessary
+            dilate(3);
+        }
+        mBmp = Bitmap.createScaledBitmap(mBmp, width, height, false);
+
+        // black white
+        blackWhiteInv();
+
+        //  skeletonize
+        skeletonize();
+
+        // dilate
+        dilate(mDilateLevel);
+
+        mWidth = width;
+        mHeight = height;
+    }
+    public void setLineWidth(int widthLevel) {
+        int dilateLv = widthLevel;
+        if (dilateLv == mDilateLevel) {
+            return;
+        }
+        mDilateLevel = dilateLv;
+        blackWhiteInv();
+        skeletonize();
+        dilate(mDilateLevel);
+    }
+
+    public SketchImage copy() {
+        SketchImage inst = new SketchImage();
+        inst.mBmpOriginal = mBmpOriginal.copy(mBmpOriginal.getConfig(), true);
+        inst.mBmp = mBmp.copy(mBmp.getConfig(), true);
+        inst.mDilateLevel = mDilateLevel;
+        return inst;
+    }
+
+    public void save(String path) {
+        save(path, false);
+    }
+
+    /**
+     *
+     * @param path
+     * @param bwInv true: bg=black, fg=white
+     *              false: bg=white, fg=black
+     */
+    public void save(String path, boolean bwInv) {
+        // TODO
+    }
+
+
+    public void invert() {
+        NativeImgProcessor processor = new NativeImgProcessor();
+        processor.invert(mBmp);
+    }
+
+
+    private void dilate(int iteration) {
+        NativeImgProcessor processor = new NativeImgProcessor();
+        processor.dilate(mBmp, iteration);
+    }
+
+    private void blackWhiteInv() {
+        blur();
+        adaptiveThreshold();
+    }
+
+    private void blur() {
+        NativeImgProcessor processor = new NativeImgProcessor();
+        processor.blur(mBmp);
+        processor.bilateralFilter(mBmp);
+    }
+
+    private void adaptiveThreshold() {
+        NativeImgProcessor processor = new NativeImgProcessor();
+        processor.blackWhiteInv(mBmp);
+    }
+
+    private void skeletonize() {
+        NativeImgProcessor processor = new NativeImgProcessor();
+        processor.skeletonize(mBmp);
+    }
+
+
+    public static SketchImage createFromPhoto(String path) throws IOException {
+        // TODO
+        SketchImage inst = new SketchImage();
+        Bitmap tmpBmp = BitmapFactory.decodeFile(path);
+        if (tmpBmp == null) {
+            throw new IOException("Get bitmap from file:" + path + " failed.");
+        }
+        inst.mBmp.createScaledBitmap(tmpBmp, WIDTH, HEIGHT, false);
+        inst.blackWhiteInv();
+        inst.skeletonize();
+        inst.mBmpOriginal = inst.mBmp.copy(inst.mBmp.getConfig(), true);
+        inst.mWidth = WIDTH;
+        inst.mHeight = HEIGHT;
+        return inst;
+    }
+
+    public static SketchImage createFromSketch(String path) throws IOException {
+        // TODO
+        SketchImage inst = new SketchImage();
+        inst.mBmp = BitmapFactory.decodeFile(path);
+        if (inst.mBmp == null) {
+            throw new IOException("Get bitmap from file:" + path + " failed.");
+        }
+        inst.resize(WIDTH, HEIGHT);
+        inst.mBmpOriginal = inst.mBmp.copy(inst.mBmp.getConfig(), true);
+        inst.mWidth = WIDTH;
+        inst.mHeight = HEIGHT;
+        return inst;
+    }
+}
---
+++ PaintHelper/app/src/main/jni/imgprocess.cpp
@@ -0,0 +1,84 @@
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "AdvMorph.h"
+#include <stdio.h>
+#include <android/log.h>
+#include <string>
+#include "com_ntu_sdp2_painthelper_capture_NativeEdgeDetector.h"
+
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,"houghcircle.cpp",__VA_ARGS__)
+
+using namespace cv;
+
+JNIEXPORT void JNICALL Java_com_ntu_sdp2_painthelper_utils_NativeImgProcessor_nativeBlur
+  (JNIEnv *env, jobject clazz, jlong imgsrc, jlong imgdst, jint kernelSize)
+{
+    Mat& img = *(Mat*) imgsrc;
+    Mat& imgOut = *(Mat*) imgdst;
+    blur(img, imgOut, Size(kernelSize, kernelSize));
+}
+
+JNIEXPORT void JNICALL Java_com_ntu_sdp2_painthelper_utils_NativeImgProcessor_nativeBilateralFilter
+  (JNIEnv *env, jobject clazz, jlong imgsrc, jlong imgdst, jint diameter)
+{
+    Mat& img = *(Mat*) imgsrc;
+    Mat& imgOut = *(Mat*) imgdst;
+    bilateralFilter(img, imgOut, diameter, diameter * 2, diameter / 2);
+}
+
+JNIEXPORT void JNICALL Java_com_ntu_sdp2_painthelper_utils_NativeImgProcessor_nativeAdaptiveThres
+  (JNIEnv *env, jobject clazz, jlong imgsrc, jlong imgdst, jint maxThres, jint methodIdx, jint typeIdx, jint blockSize, jint C)
+{
+    Mat& img = *(Mat*) imgsrc;
+    Mat& imgOut = *(Mat*) imgdst;
+
+    int method = ((methodIdx == 0) ? ADAPTIVE_THRESH_MEAN_C : ADAPTIVE_THRESH_GAUSSIAN_C);
+    int type = ((typeIdx == 0) ? THRESH_BINARY : THRESH_BINARY_INV);
+    adaptiveThreshold(img, imgOut, maxThres, method, type, blockSize, C);
+}
+
+JNIEXPORT void JNICALL Java_com_ntu_sdp2_painthelper_utils_NativeImgProcessor_nativeSkeletonize
+  (JNIEnv *env, jobject clazz, jlong imgsrc, jlong imgdst);
+{
+    Mat& img = *(Mat*) imgsrc;
+    Mat& imgOut = *(Mat*) imgdst;
+
+    AdvMorph m;
+    IplImage iplImg = IplImage(img);
+    IplImage iplImgOut = IplImage(imgOut);
+    m.Thin(&iplImg, &iplImgOut);
+}
+
+JNIEXPORT void JNICALL Java_com_ntu_sdp2_painthelper_utils_NativeImgProcessor_nativeDilate
+  (JNIEnv *env, jobject clazz, jlong imgsrc, jlong imgdst, jint iteration, jint kernelIdx, jint kernelN);
+{
+    Mat& img = *(Mat*) imgsrc;
+    Mat& imgOut = *(Mat*) imgdst;
+    Mat tmp;
+    int dilateType;
+    switch (kernelIdx) {
+	case 0: dilateType = MORPH_RECT; break;
+	case 1: dilateType = MORPH_CROSS; break;
+	case 2: dilateType = MORPH_ELLIPSE; break;
+	default: dilateType = MORPH_RECT;
+    }
+
+    Mat kernel = getStructuringElement(dilateType,
+            Size(2 * kernelN + 1, 2 * kernelN + 1),
+            Point(kernelN, kernelN));
+
+    img.copyTo(tmp);
+    for (int i = 0; i < iteration; i++) {
+        dilate(tmp, imgOut, kernel);
+        imgOut.copyTo(tmp);
+    }
+}
+
+JNIEXPORT void JNICALL Java_com_ntu_sdp2_painthelper_utils_NativeImgProcessor_nativeInvert
+  (JNIEnv *env, jobject clazz, jlong imgsrc, jlong imgdst);
+{
+    Mat& img = *(Mat*) imgsrc;
+    Mat& imgOut = *(Mat*) imgdst;
+    invert(img, imgOut);
+}
